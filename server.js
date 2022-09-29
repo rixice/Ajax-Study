3e6b741e65d7ba96a7aa3d4148793b28bf392b9b
bear <43441591+rixice@users.noreply.github.com>
2022-09-29 10:43:26 +0800

    commited

--- server.js
+++ server.js
@@ -17,26 +17,48 @@ app.get('/server', (request, response)=>{
 });
 
 app.post('/server', (request, response)=>{
-    // 设置响应头, 设置允许跨域
     response.setHeader('Access-Control-Allow-Origin', '*')
     response.setHeader('Access-Control-Allow-Headers', '*')
-    // 设置响应
+
     response.send('Hello AJAX, you are post')
 });
 
 app.all('/json-server', (request, response)=>{
-    // 设置响应头, 设置允许跨域
     response.setHeader('Access-Control-Allow-Origin', '*')
     response.setHeader('Access-Control-Allow-Headers', '*')
     // 响应一个数据
     const data = {
-        msg: 'Hello World'
+        msg: 'Hello World !'
     }
     // 对 对象 进行 字符串转换
     let str = JSON.stringify(data)
     // 设置响应
     // response.send('Hello AJAX')
     response.send(str)
+});
+
+app.get('/ie-server', (request, response)=>{
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    response.setHeader('Access-Control-Allow-Headers', '*')
+
+    response.send('Hello IE')
+});
+
+// 延时响应
+app.get('/delay-server', (request, response)=>{
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    setTimeout(()=>{
+        response.send('Hello delay')
+    },4000)
+});
+
+// jQuery服务
+app.all('/jq', (request, response)=>{
+    const data = {name: 'Hello World'}
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    response.setHeader('Access-Control-Allow-Headers', '*')
+    // response.send('Hello jQuery AJAX')
+    response.send(JSON.stringify(data))
 });
 
 // 4. 监听端口启动服务
